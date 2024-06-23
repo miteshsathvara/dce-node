9d35251184ef2f7a9f481da1df70b6a200da0549
“Mitesh <mitesh0267@gmail.com>
2024-06-23 22:19:40 +0530

    start exam detail api changes

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,9 +2,16 @@ const db = require("../models");
 const User = db.user;
 
 exports.getUserExamDetail = async (req, res) => {
-    console.log(req.userId);
-    // User.findById(req.userId, function(err, user) {
-    //     res.send(user);
-    //  });
-    return res.status(200).send({ message: req.userId});
+    User.findByPk(req.userId)
+    .then(data => {
+        res.status(200).send({
+            message: "Success",
+            data : data
+        });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving User with id=" + req.userId
+      });
+    });
 }
